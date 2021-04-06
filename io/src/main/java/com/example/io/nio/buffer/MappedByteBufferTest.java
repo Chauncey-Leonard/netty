cc2b6fd73e3ef892dec775de134a7f2d42d5b9fb
Chauncey <1723446@qq.com>
2021-04-06 09:24:09 +0800

    优化MappedByteBuffer代码

--- io/src/main/java/com/example/io/nio/buffer/MappedByteBufferTest.java
+++ io/src/main/java/com/example/io/nio/buffer/MappedByteBufferTest.java
@@ -16,6 +16,7 @@ public class MappedByteBufferTest {
         RandomAccessFile raf = new RandomAccessFile("test.txt", "rw");
         // 获取对应的通道
         FileChannel channel = raf.getChannel();
+
         /*
          * 参数一：使用的读写模式
          * 参数二：可以直接修改的位置
@@ -23,8 +24,9 @@ public class MappedByteBufferTest {
          */
         MappedByteBuffer mappedByteBuffer = channel.map(FileChannel.MapMode.READ_WRITE,
                 0, 5);
-        mappedByteBuffer.put(1, (byte) 'H');
+        mappedByteBuffer.put(0, (byte) 'H');
         mappedByteBuffer.put(3, (byte) '9');
+        channel.close();
         raf.close();
     }
 
