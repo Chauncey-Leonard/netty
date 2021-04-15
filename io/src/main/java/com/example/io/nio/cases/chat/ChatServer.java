0ddb41b6a5aae74004e2c75085f6613ea01f986c
Chauncey <1723446@qq.com>
2021-04-15 11:08:20 +0800

    服务端注册到选择器中并监听客户端的连接

--- io/src/main/java/com/example/io/nio/cases/chat/ChatServer.java
+++ io/src/main/java/com/example/io/nio/cases/chat/ChatServer.java
@@ -1,5 +1,13 @@
 package com.example.io.nio.cases.chat;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
 /**
  * NIO 网络编程应用实例：群聊系统
  * <p>
@@ -13,4 +21,74 @@ package com.example.io.nio.cases.chat;
  * @author Chauncey
  */
 public class ChatServer {
+
+    private Selector selector;
+
+    private static final int PORT = 8888;
+
+    private ServerSocketChannel serverSocketChannel;
+
+    /**
+     * 初始化
+     */
+    public ChatServer() {
+        try {
+            // 获取选择器
+            selector = Selector.open();
+            // 获取通道对象
+            serverSocketChannel = ServerSocketChannel.open();
+            // 绑定端口
+            serverSocketChannel.socket().bind(new InetSocketAddress(PORT));
+            // 设置非阻塞模式
+            serverSocketChannel.configureBlocking(false);
+            // 将通道注册到 Selector 中
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listen() {
+        try {
+            while (true) {
+                int count = selector.select(2000);
+                if (count > 0) {
+                    // 获取SelectionKey集合
+                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey key = iterator.next();
+
+                        // 监听到accept
+                        if (key.isAcceptable()) {
+                            SocketChannel sc = serverSocketChannel.accept();
+                            // 将连接到的客户端注册到 Selector 中
+                            sc.register(selector, SelectionKey.OP_READ);
+                            // 提示
+                            System.out.println(sc.getRemoteAddress() + " 上线 ");
+                        }
+
+                        if (key.isReadable()) {
+                            // 处理读
+                        }
+
+                        // 删除当前的key，防止重复处理
+                        iterator.remove();
+                    }
+                } else {
+                    System.out.println("暂无事件……");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readData() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
